95e94a6acd1587d1155e17fb4cef27178df4b765
lsa50-ua <lsa50@alu.ua.es>
2026-03-02 02:43:26 +0100

    Cambios finales capstone

--- launch-infra.ps1
+++ launch-infra.ps1
@@ -1,9 +1,5 @@
-# 1. Esperar a que el borrado termine (por si acaso)
-Write-Host "Limpiando rastros anteriores..."
-aws cloudformation wait stack-delete-complete --stack-name proyecto-alta-disponibilidad --region us-east-1
-
-# 2. Lanzar el despliegue limpio
-Write-Host "Lanzando despliegue de Infraestructura Privada..."
+# Script para lanzar el despliegue de la infraestructura
+Write-Host "Lanzando despliegue de Infraestructura..."
 aws cloudformation deploy `
   --template-file infraestructura.yaml `
   --stack-name proyecto-alta-disponibilidad `
--- progreso-deployment.ps1
+++ progreso-deployment.ps1
@@ -1,2 +1,4 @@
+# Script para verificar el progreso del despliegue
+Write-Host "Verificando el estado del despliegue..."
 aws cloudformation describe-stacks --stack-name proyecto-alta-disponibilidad --region us-east-1 --query "Stacks[0].StackStatus" --output text
 $env:AWS_PAGER = ""; aws cloudformation describe-stacks --region us-east-1 --stack-name proyecto-alta-disponibilidad --query "Stacks[0].Outputs"
